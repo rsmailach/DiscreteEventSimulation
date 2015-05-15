d5d675230e057735d59fec4ec28217b8773b9d52
Rachel Mailach <rachel.mailach@gmail.com>
2015-05-15 14:38:31 -0400

    Split into modules

---
+++ simulation.py
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------------#
+# simulate.py
+#  
+# This class is used to simulate the specified model.
+# 
+# Rachel Mailach
+#----------------------------------------------------------------------------------#
+
+class Model():
+	def __init__(self):
+		pass
+
+	def Run():
+		print "I ran!"
